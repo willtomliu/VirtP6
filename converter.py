24d2b8c90ac29d23b7a4775756e9561ba368f3d8
Bingjiang Wang <biw005@ucsd.edu>
2022-03-05 22:46:19 -0800

    part b

--- converter.py
+++ converter.py
@@ -5,30 +5,74 @@ import shutil
 
 from PIL import Image
 
-def convert_mp4_to_jpgs(path):
-    video_capture = cv2.VideoCapture(path)
+
+def decode(resourcePath, outFolder):
+    video_capture = cv2.VideoCapture(resourcePath)
     still_reading, image = video_capture.read()
     frame_count = 0
 
-    if os.path.exists("output"):
-        shutil.rmtree("output")
-    try:
-        os.mkdir("output")
-    except IOError:
-        print("Error occurred creating output folder")
-        return
-
     while still_reading:
-        cv2.imwrite(f"output/frame_{frame_count:04d}.jpg", image)
+        cv2.imwrite(f"{outFolder}/frame_{frame_count:04d}.jpg", image)
         # read next image
         still_reading, image = video_capture.read()
         frame_count += 1
 
-def scale_image(frame_folder, width=None, height=None):
-    images = glob.glob(f"{frame_folder}/*.jpg")
+
+# def select(inFolder, outFolder, inType, outType):
+#     images = glob.glob(f"{inFolder}/*.{inType}")
+#     images.sort()
+#     if not images:
+#         raise RuntimeError("jpg files required!")
+#     skip = 10
+#     selectedIndex = []
+#     for index, image in enumerate(images):
+#         if index % skip == 0:
+#             selectedIndex.append(index)
+#     return selectedIndex
+
+
+def select(inFolder, outFolder, inType, outType=None):
+    images = glob.glob(f"{inFolder}/*.{inType}")
     images.sort()
     if not images:
         raise RuntimeError("jpg files required!")
+    skip = 10
+    for index, image in enumerate(images):
+        if index % skip == 0:
+            _, fileName = os.path.split(image)
+            shutil.copy(image, os.path.join(outFolder, fileName))
+
+
+# def filter(inFolder, outFolder, inType, outType, selectedIndex, width=None,
+#            height=None):
+#     images = glob.glob(f"{inFolder}/*.{inType}")
+#     images.sort()
+#     if not images:
+#         raise RuntimeError("jpg files required!")
+#     first_image = Image.open(images[0])
+#     w, h = first_image.size
+#     if width and height:
+#         max_size = (width, height)
+#     elif width:
+#         max_size = (width, h)
+#     elif height:
+#         max_size = (w, height)
+#     else:
+#         raise RuntimeError('Width or height required!')
+#     for count, index in enumerate(selectedIndex):
+#         img = Image.open(images[index])
+#         img.resize(max_size, Image.ANTIALIAS)
+#         outImagePath = os.path.join(outFolder,
+#                                     f"frame{count:04d}_resized.{outType}")
+#         img.save(outImagePath)
+#         img.close()
+
+
+def filter(inFolder, outFolder, inType, outType, width=None, height=None):
+    images = glob.glob(f"{inFolder}/*.{inType}")
+    images.sort()
+    if not images:
+        raise RuntimeError(f"{inType} files required!")
     first_image = Image.open(images[0])
     w, h = first_image.size
     if width and height:
@@ -39,30 +83,47 @@ def scale_image(frame_folder, width=None, height=None):
         max_size = (w, height)
     else:
         raise RuntimeError('Width or height required!')
-    for image in images:
+    for count, image in enumerate(images):
         img = Image.open(image)
-        img.thumbnail(max_size, Image.ANTIALIAS)
-        prefix, suffix = image.split('.')
-        output_image_path = '.'.join([prefix + '_resized', suffix])
-        img.save(output_image_path)
+        img.resize(max_size, Image.ANTIALIAS)
+        outImagePath = os.path.join(outFolder,
+                                    f"frame{count:04d}_resized.{outType}")
+        img.save(outImagePath)
         img.close()
 
 
-def make_gif(frame_folder, filename):
-    images = glob.glob(f"{frame_folder}/*_resized.jpg")
+def output(inFolder, outputPath, inType, outType):
+    images = glob.glob(f"{inFolder}/*{inType}")
     images.sort()
     if not images:
-        raise RuntimeError("jpg files required!")
+        raise RuntimeError(f"{inType} files required!")
     frames = [Image.open(image) for image in images]
     frame_one = frames[0]
-    frame_one.save(filename, format="GIF", append_images=frames,
+    frame_one.save(outputPath, format=outType, append_images=frames,
                    save_all=True, duration=50, loop=0)
 
-name = "flask_demo"
-mp4_path = f"{name}.mp4"
-frame_folder = "output"
-filename = f"{name}.gif"
-convert_mp4_to_jpgs(mp4_path)
-scale_image(frame_folder, 800)
-make_gif(frame_folder, filename)
 
+def main():
+    resourceFolder = "resource"
+    resourceName = "flask_demo"
+    resourceType = "mp4"
+    resourcePath = os.path.join(resourceFolder,
+                                f"{resourceName}.{resourceType}")
+
+    mem1 = "mem1"
+    mem2 = "mem2"
+    mem3 = "mem3"
+
+    outputFolder = "output"
+    outputName = resourceName
+    outputType = "gif"
+    outputPath = os.path.join(outputFolder, f"{outputName}.{outputType}")
+
+    decode(resourcePath, mem1)
+    select(mem1, mem2, "jpg")
+    filter(mem2, mem3, "jpg", "jpg", 800)
+    output(mem3, outputPath, "jpg", "GIF")
+
+
+if __name__ == '__main__':
+    main()
