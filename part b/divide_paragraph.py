09abdd327185b80d0e09b87a8371f8fd7d326cee
ly <willtom_liu@163.com>
2022-03-07 01:22:08 -0800

    Fix divide bug

--- part b/divide_paragraph.py
+++ part b/divide_paragraph.py
@@ -4,8 +4,9 @@ import copy
 indent = '    '
 indent_length = 4
 
+
 class CodeParagrah:
-    def __init__(self, start, end, indent_num, children = []):
+    def __init__(self, start, end, indent_num, children):
         self.start = start
         self.end = end
         self.indent_num = indent_num
@@ -15,43 +16,50 @@ class CodeParagrah:
 def count_indent(code):
     count = 0
     while code.startswith(indent):
-        count+=1
+        count += 1
         code = code[4:]
     return count
 
+
+def isBlank(code):
+    a = code.strip() == ''
+    return a
+
+
 def find_paragraph(result):
-    result += '\n'
+    result.append('END')
     paragraph_stack = []
     for i in range(len(result)):
         code = result[i]
         indent_num = count_indent(code)
-        line_num = i+1
+        line_num = i + 1
+        if isBlank(code):
+            continue
+
         if len(paragraph_stack) == 0 or indent_num >= paragraph_stack[-1][1]:
-            paragraph_stack.append( [line_num, indent_num, None] )
+            paragraph_stack.append([line_num, indent_num, []])
         else:
             pre_indent_num = paragraph_stack[-1][1]
-            current_paragraph = CodeParagrah(-1, line_num, pre_indent_num)
+            current_paragraph = CodeParagrah(-1, line_num, pre_indent_num, [])
             while len(paragraph_stack) > 0 and indent_num < paragraph_stack[-1][1]:
-                if  paragraph_stack[-1][1] != pre_indent_num:
+                if paragraph_stack[-1][1] != pre_indent_num:
                     current_paragraph.start = paragraph_stack[-1][0]
-                    paragraph_stack[-1][2] = copy.deepcopy(current_paragraph)
+                    paragraph_stack[-1][2].append(copy.deepcopy(current_paragraph))
 
                     pre_indent_num = paragraph_stack[-1][1]
                     current_paragraph = CodeParagrah(-1, line_num, pre_indent_num, paragraph_stack[-1][2])
 
                 else:
-                    if paragraph_stack[-1][2] != None:
-                        current_paragraph.children.append(paragraph_stack[-1][2])
-
+                    current_paragraph.children += paragraph_stack[-1][2]
                 paragraph_stack.pop()
-            current_paragraph.start = paragraph_stack[-1][0]
-            paragraph_stack[-1][2] = copy.deepcopy(current_paragraph)
-    return paragraph_stack
 
-f = open("gif_maker.py")
+            current_paragraph.start = paragraph_stack[-1][0]
+            paragraph_stack[-1][2].append(copy.deepcopy(current_paragraph))
+            paragraph_stack.append([line_num, indent_num, []])
+    return paragraph_stack[0:-1]
+
+
+f = open("nn.py")
 lines = f.readlines()
 result = find_paragraph(lines)
 a = 1
-
-
-
